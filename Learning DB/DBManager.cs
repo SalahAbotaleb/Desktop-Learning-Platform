43e29b9fab43e818a5105fba7436a32ad68b3783
Moaaz Tarek <moaaztarek@hotmail.com>
2022-12-02 18:58:05 +0200

    database manager file

---
+++ Learning DB/DBManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public class DBManager
+    {
+        static string DB_Connection_String = @"Data Source=learningplat.database.windows.net;Initial Catalog=""Learning Platform"";Persist Security Info=True;User ID=MOA;Password=Password123";
+        SqlConnection myConnection;
+
+        public DBManager()
+        {
+            myConnection = new SqlConnection(DB_Connection_String);
+            try
+            {
+                myConnection.Open();
+                Console.WriteLine("The DB connection is opened successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The DB connection is failed");
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            try
+            {
+                SqlCommand myCommand = new SqlCommand(query, myConnection);
+                return myCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        public DataTable ExecuteReader(string query)
+        {
+            try
+            {
+                SqlCommand myCommand = new SqlCommand(query, myConnection);
+                SqlDataReader reader = myCommand.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    reader.Close();
+                    return dt;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public object ExecuteScalar(string query)
+        {
+            try
+            {
+                SqlCommand myCommand = new SqlCommand(query, myConnection);
+                return myCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                myConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        
+    }
+}
+;
