38a6d558e19d9a0047b3f4f895677f26179daba9
Omar Elzahar <omarshelzahar@gmail.com>
2022-12-10 01:08:13 +0200

    maximize button locked

--- Learning DB/Form1.Designer.cs
+++ Learning DB/Form1.Designer.cs
@@ -288,6 +288,7 @@
             this.Controls.Add(this.kryptonButton2);
             this.Controls.Add(this.kryptonButton1);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Palette = this.kryptonPalette1;
             this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
