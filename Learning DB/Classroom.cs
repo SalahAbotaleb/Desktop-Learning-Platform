6b31a991244d1f854f6c701656796eedab95088b
Omar Elzahar <omarshelzahar@gmail.com>
2022-12-26 04:44:05 +0200

    Classroom GUI

--- Learning DB/Classroom.cs
+++ Learning DB/Classroom.cs
@@ -9,10 +9,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
 
 namespace Learning_DB
 {
-    public partial class Classroom : Form
+    public partial class Classroom : KryptonForm
     {
         Controller c;
         int Classroom_ID;
